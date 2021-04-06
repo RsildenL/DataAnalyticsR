eee666e340f4694c2c6f09185156dd3d9c3d9dae
afonsompenalva <71217100+afonsompenalva@users.noreply.github.com>
2021-04-06 17:34:25 +0100

    Update cashless payments.R

--- cashless payments.R
+++ cashless payments.R
@@ -26,19 +26,13 @@ profit<-sum(tdp$margin)
 #"The National Retail Association suggests that the swipe fees are usually between 2% and 4% per transaction and can vary depending on a merchants’ card volume and other factors."
 #"Kids dont have cards"
 #cost is divided by 365 to see daily
-
-#daily_profit<- product_data[, margin:=(price/(1+tax_rate) - cost)]
-#tdp = merge(transaction_data,daily_profit,by='product_name',all.x=T)
 cost_cl_hardware<- uniqueN(merged_data$machine) * 240
-
-
 n_products<-tdp[,.N, by= product_name]
 n_products[, n:= 1.22*N]
-
 cl=merge(n_products,tdp[,.(product_name,margin)],by='product_name', all.x = T)
 cl=cl[!duplicated(product_name),.(product_name,N,n,margin,new_added_margin=(n-N)*margin), ]
-
 profit_cl<- sum(cl$new_added_margin)-cost_cl_hardware
+profit_cl+profit
 percentage_increase_cl<- profit_cl/profit
 
 # next quarter
@@ -51,29 +45,17 @@ percentage_increase_cl<- profit_cl/profit
 
 #with the other cost structure
 
-#daily_sales_cl_fee<-merged_data[ , items_machine_day_cl:=items_machine_day*1.22, by = .(machine,date)]
-
-#daily_profit_cl_fee<- merged_data[, margin_cl_fee:=(price/(1+tax_rate)*items_machine_day_cl) - (cost*items_machine_day_cl)- (0.03*price/(1+tax_rate)*items_machine_day_cl), by=.(machine, date)]
-
-#profit_cl_fee<- sum(daily_profit_cl_fee$margin_cl_fee)
-
 daily_profit_fee<- product_data[, margin_fee:=(price/(1+tax_rate) - cost-0.03*price)]
 tdp_fee = merge(transaction_data,daily_profit,by='product_name',all.x=T)
-
-
 n_products<-tdp_fee[,.N, by= product_name]
 n_products[, n:= 1.22*N]
 cl_fee=merge(n_products,tdp_fee[,.(product_name,margin_fee)],by='product_name', all.x = T)
 cl_fee=cl_fee[!duplicated(product_name),.(product_name,N,n,margin_fee,new_added_margin_fee=(n-N)*margin_fee), ]
 
 profit_cl_fee<- sum(cl_fee$new_added_margin_fee)
+profit_cl_fee+profit
 percentage_increase_cl_fee<- profit_cl_fee/profit
 
-
-
-# increase in revenue
-profit_diff_fee<- profit_cl_fee-profit
-percentage_increase_fee<- profit_diff_fee/profit
 
 
 
